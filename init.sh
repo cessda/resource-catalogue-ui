244bec7dc6613d411dfb68e31598e2fc7f9b0b9a
Konstantinos Spyrou <23262569+spyroukostas@users.noreply.github.com>
2026-05-26 15:32:13 +0300

    chore: add Docker Compose example

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst '${PLATFORM_API_ENDPOINT} ${FAQ_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
+envsubst '${API_BASE_URL}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
 nginx -t
 nginx -g "daemon off;"
